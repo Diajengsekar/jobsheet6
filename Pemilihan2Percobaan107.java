3304b6528c8525792b061955c08b8d16c1e806ec
diajengsekararum31@gmail.com <diajengsekararum31@gmail.com>
2023-10-05 12:21:51 +0700

    percobaan1, per 1, no3

--- Pemilihan2Percobaan107.java
+++ Pemilihan2Percobaan107.java
@@ -6,7 +6,7 @@ public class Pemilihan2Percobaan107{
         System.out.println("Masukkan tahun");
         int tahun = input07.nextInt();
 
-        if ((tahun % 4) == 0){
+        if ((tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0)){
             if ((tahun % 100) != 0)
                 System.out.println("Tahun kabisat");        
         } else
