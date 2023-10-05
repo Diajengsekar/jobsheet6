b32af01746ce0171691c44825906332c98073519
diajengsekararum31@gmail.com <diajengsekararum31@gmail.com>
2023-10-05 13:51:34 +0700

    per2, pertanyaan3

--- Pemilihan2Percobaan207.java
+++ Pemilihan2Percobaan207.java
@@ -12,13 +12,16 @@ public class Pemilihan2Percobaan207 {
         float totalSudut;
         totalSudut = sudut1 + sudut2 + sudut3;
 
-        if (totalSudut == 180) {
-            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
-               System.out.println( "Segitiga tersebut adalah segitiga siku siku");
-            else
-            System.out.println("Segitiga tersebut bukan segitiga siku siku");   
-        } else
-            System.out.println("Bukan segitiga");
-    }
 
-}
+            if (sudut1 == sudut2 && sudut2 == sudut3) {
+                System.out.println("Segitiga adalah segitiga sama sisi.");
+            } else if (sudut1 == sudut2 || sudut2 == sudut3 || sudut1 == sudut3) {
+                System.out.println("Segitiga adalah segitiga sama kaki.");
+            } else {
+                System.out.println("Segitiga adalah segitiga sembarang.");
+            }
+        }
+        }
+    
+
+
